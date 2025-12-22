03c8646a2360d63f061f9717feb53098e6bf0acc
ubozkurt <ubozkurt@localhost>
2025-12-22 21:55:04 +0300

    scripts added

---
+++ scripts/start.bat
@@ -0,0 +1,9 @@
+@echo off
+taskkill -f -im mongod.exe
+taskkill /f /im nomad.exe
+taskkill /f /im consul.exe
+taskkill /f /fi "WINDOWTITLE eq Node-*"
+taskkill /f /fi "WINDOWTITLE eq Node-*"
+for %%l in (1 2 3) do (
+    start /min /D %~dp0 cmd /c start-agent.bat %%l %1
+)
